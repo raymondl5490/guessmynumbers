437759963b3766d3b707d56542168c59d8856053
raymondkdev <raymondkdev@gmail.com>
2022-06-20 08:10:21 -0400

    rename Admin route from home to admin

--- routes/web.php
+++ routes/web.php
@@ -26,7 +26,7 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/admin', [App\Http\Controllers\AdminController::class, 'index'])->name('admin');
 
 Route::post('players', [PlayerController::class, 'store'])->name('players.store');
 
