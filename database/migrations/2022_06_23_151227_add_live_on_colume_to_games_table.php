dbba105e1abe972d195ee80c20da2e7605922d50
Raymond <raymondl5490@gmail.com>
2022-06-23 11:41:18 -0400

    update migration replace game_start and game_end to live_on on games table

---
+++ database/migrations/2022_06_23_151227_add_live_on_colume_to_games_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('games', function (Blueprint $table) {
+            //
+            $table->string('live_on')->after('number_three');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('games', function (Blueprint $table) {
+            //
+            $table->dropColumn('live_on');
+        });
+    }
+};
