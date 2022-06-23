2e8932b4c9d211b59966cb1130df2068a06bcaad
raymondkdev <raymondkdev@gmail.com>
2022-06-23 11:41:18 -0400

    update migration replace game_start and game_end to live_on on games table

---
+++ database/migrations/2022_06_23_150042_remove_game_start_column_from_games_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('games', function (Blueprint $table) {
+            //
+            $table->dropColumn('game_start');
+            $table->dropColumn('game_end');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('games', function (Blueprint $table) {
+            //
+            $table->dateTime('game_start')->nullable();
+            $table->dateTime('game_end')->nullable();
+        });
+    }
+};
