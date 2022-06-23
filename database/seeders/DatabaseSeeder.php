daa3bd944b75635e9b0c323c28b7d82b15844f20
raymondkdev <raymondkdev@gmail.com>
2022-06-23 12:20:00 -0400

    add a user and game on seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,10 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+use Carbon\Carbon;
 
 class DatabaseSeeder extends Seeder
 {
@@ -15,5 +19,33 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+
+        /**
+         * Super user Josh Gottsegen, josh@onelightstudios.com, password: password
+         */
+        $users = DB::table('users')->count();
+        if (!$users) {
+            DB::table('users')->insert([
+                'name' => 'Josh',
+                'email' => 'josh@onelightstudios.com',
+                'password' => Hash::make('password'),
+            ]);
+        }
+
+        $games = DB::table('games')->count();
+        if (!$games) {
+            DB::table('games')->insert([
+                'number_one' => 3,
+                'number_two'=> 2,
+                'number_three' => 2,
+                'live_on' => Carbon::now('America/Los_Angeles')->format('Y-m-d a'),
+                'author_name' => 'Smart',
+                'author_location' => 'CA',
+                'author_email' => 'smartdev0322@gmail.com',
+                'link' => 'https://github.com',
+                'link_title' => 'visit me',
+            ]);
+        }
+
     }
 }
