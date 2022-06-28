2580e777e87b404523a24d56f8f7a1608b71b5d3
Raymond <raymondl5490@gmail.com>
2022-06-28 18:03:08 -0400

    fix: not closing help modal at startup

--- resources/js/components/modals/HelpModalComponent.vue
+++ resources/js/components/modals/HelpModalComponent.vue
@@ -11,36 +11,36 @@
             How to Play
         </template>
         <template #content>
-            <p class="text-md my-8">
+            <p class="my-8 text-md">
                 You have three tries to guess the featured number.
                 Press the check mark to submit.
             </p>
 
-            <p class="text-md my-8">
+            <p class="my-8 text-md">
                 After each guess, the colors will change to show how close you got!
             </p>
 
-            <p class="text-md my-8">
+            <p class="my-8 text-md">
                 If you guess correctly, <span class="font-bold">you have a chance to submit your own numbers to be featured!</span>
             </p>
 
-            <p class="text-md my-8">
+            <p class="my-8 text-md">
                 <img :src="asset('images/help_green.png')" class="w-2/3 mx-auto" />
                 <span class="font-bold">Green</span> indicates the number is in the proper spot.
             </p>
 
-            <p class="text-md my-8">
+            <p class="my-8 text-md">
                 <img :src="asset('images/help_yellow.png')" class="w-2/3 mx-auto" />
                 <span class="font-bold">Yellow</span> indicates the number is correct, but in the wrong spot.
             </p>
 
-            <p class="text-md my-8">
+            <p class="my-8 text-md">
                 <img :src="asset('images/help_double.png')" class="w-2/3 mx-auto" />
                 A <span class="font-bold">Purple</span> line indicates the number is used multiple times.
             </p>
 
             <hr/>
-            <p class="text-md font-light text-center">
+            <p class="font-light text-center text-md">
                 <span class="font-bold">New Numbers updated every 12 hours!</span>
                 <br/>
                 12:00am PST / 12:00pm PST
@@ -67,7 +67,7 @@ export default {
     methods: {
         onClose() {
             this.$emit('close')
-        }
+        },
     }
 }
 </script>
