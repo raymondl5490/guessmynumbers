6050bd76697802755a4c18eecd658d8fb09cce82
raymondkdev <raymondkdev@gmail.com>
2022-06-27 20:00:04 -0400

    statistics on api, admin and frontend

--- app/Http/Controllers/CurrentGameController.php
+++ app/Http/Controllers/CurrentGameController.php
@@ -3,15 +3,28 @@
 namespace App\Http\Controllers;
 
 use App\Http\Resources\GameResource;
+use App\Models\Attempt;
 use App\Models\Game;
+use Illuminate\Http\JsonResponse;
 
 class CurrentGameController extends Controller
 {
     /**
-     * @return GameResource
+     * @return JsonResponse
      */
-    public function __invoke(): GameResource
+    public function __invoke(): JsonResponse
     {
-        return new GameResource(Game::currentlyActive()->first());
+        $activeGame = Game::currentlyActive()->first();
+        $numberOfAttempts = Attempt::where('game_id', $activeGame->id)
+            ->count();
+        $numberOfWons = Attempt::where('game_id', $activeGame->id)
+            ->where('won', 1)
+            ->count();
+
+        $activeGameJson = (new GameResource($activeGame))->jsonSerialize();
+        $activeGameJson['number_of_attempts'] = $numberOfAttempts;
+        $activeGameJson['number_of_wons'] = $numberOfWons;
+
+        return response()->json($activeGameJson);
     }
 }
--- resources/js/components/modals/StatisticsModalComponent.vue
+++ resources/js/components/modals/StatisticsModalComponent.vue
@@ -9,15 +9,15 @@
             </div>
         </template>
         <template #content>
-            <div class="w-full grid grid-cols-2 gap-8 my-5">
+            <div class="grid w-full grid-cols-2 gap-8 my-5">
                 <div class="col-span-1 text-center">
-                    <div class="text-center text-3xl font-bold">
-                        {{ gamesPlayed }}
+                    <div class="text-3xl font-bold text-center">
+                        {{ currentGame.number_of_attempts }}
                     </div>
                     Rounds Played
                 </div>
                 <div class="col-span-1 text-center">
-                    <div class="text-center text-3xl font-bold">
+                    <div class="text-3xl font-bold text-center">
                         {{ currentRoundWinPercentage }}
                     </div>
                     Current Round Global Win %
@@ -27,13 +27,13 @@
             <hr class="my-3" />
 
             <div>
-                <h3 class="text-xl font-medium text-center mb-8">
+                <h3 class="mb-8 text-xl font-medium text-center">
                     YOUR GUESSES
                 </h3>
                 <GameComponent :hide-numbers="true" />
             </div>
 
-            <div class="w-full grid grid-cols-2 gap-8 my-5">
+            <div class="grid w-full grid-cols-2 gap-8 my-5">
                 <div class="text-center">
                     <h3 class="text-lg font-light">
                         NEW NUMBER IN
@@ -44,7 +44,7 @@
                 </div>
                 <div class="flex flex-col justify-center">
                     <button
-                        class="bg-green-600 hover:bg-green-900 rounded-md px-5 py-3 w-full border-0 text-md text-white"
+                        class="w-full px-5 py-3 text-white bg-green-600 border-0 rounded-md hover:bg-green-900 text-md"
                     >
                        SHARE
                     </button>
@@ -63,6 +63,8 @@
 import ModalComponent from "../ui/ModalComponent";
 import IconComponent from "../ui/IconComponent";
 import GameComponent from "../GameComponent";
+import { mapState } from "pinia";
+import { useGameStore } from "../../store";
 export default {
     components: {GameComponent, IconComponent, ModalComponent},
     props: {
@@ -73,9 +75,16 @@ export default {
     },
     data() {
         return {
-            gamesPlayed: 3,
-            currentRoundWinPercentage: '42%',
         }
+    },
+    computed: {
+        ...mapState(useGameStore, ['currentGame']),
+        currentRoundWinPercentage() {
+            const number_of_attempts = this.currentGame.number_of_attempts;
+            const number_of_wons = this.currentGame.number_of_wons;
+            if (!number_of_attempts) return 0;
+            return _.max(_.round(number_of_wons / number_of_attempts * 100, 0), 100);
+        },
     },
     methods: {
         onClose() {
