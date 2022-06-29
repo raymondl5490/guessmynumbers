7776b8e888927ca41230279181432a8449146ae7
raymondkdev <raymondkdev@gmail.com>
2022-06-29 17:25:43 -0400

    styling on numbers

--- resources/js/mixins/NumberUsageMixin.vue
+++ resources/js/mixins/NumberUsageMixin.vue
@@ -9,7 +9,7 @@ import {useAttemptStore, useGuessStore} from "../store";
 export default {
     computed: {
         ...mapState(useAttemptStore, ['correctNumbers']),
-        ...mapState(useGuessStore, ['existingGuessNumbers', 'guess', 'board']),
+        ...mapState(useGuessStore, ['submittedGuessNumbers', 'guess', 'board']),
     },
     methods: {
         isNumberOnBoard(number) {
@@ -19,15 +19,10 @@ export default {
         isNumberCorrect(number) {
             return includes(this.correctNumbers, number);
         },
-        // correntNumber is not provided when this function called from InputNumbersComponent
-        isNumberInCorrectSpot(number, correctNumber = null) {
-            if (correctNumber !== null) {
-                return number === correctNumber
-            }
-
+        isNumberInCorrectSpot(number) {
             const correctIndex = indexOf(this.correctNumbers, number);
             let inCorrectSpot = false;
-            forEach(this.existingGuessNumbers, existingNumbers => {
+            forEach(this.submittedGuessNumbers, existingNumbers => {
                 if (existingNumbers[correctIndex] === number) {
                     inCorrectSpot = true;
                 }
@@ -36,37 +31,10 @@ export default {
             return inCorrectSpot;
         },
         isNumberUsedMultipleTimes(number) {
-            return filter(this.correctNumbers, number).length > 1;
+            return _.countBy(this.correctNumbers, (n) => n == number ? 'count' : 'miss')['count'] > 1;
         },
         hasNumberBeenSubmitted(number) {
-            let submitted = false;
-            forEach(this.existingGuessNumbers, existingNumbers => {
-                if (includes(existingNumbers, number)) {
-                    submitted = true;
-                }
-            });
-
-            return submitted;
-        },
-        // correntNumber is not provided when this function called from InputNumbersComponent
-        getNumberStyle(number, isCurrentGuess = false, correctNumber = null) {
-            if (!this.isNumberOnBoard(number)) {
-                return '';
-            }
-
-            if (!this.hasNumberBeenSubmitted(number) || isCurrentGuess) {
-                return 'bg-gray-500 text-white';
-            }
-
-            if (this.isNumberCorrect(number) && this.isNumberInCorrectSpot(number, correctNumber)) {
-                return 'bg-green-600 text-white';
-            }
-
-            if (this.isNumberCorrect(number) && !this.isNumberInCorrectSpot(number, correctNumber)) {
-                return 'bg-yellow-500 text-white';
-            }
-
-            return 'bg-gray-500 text-white';
+            return _.includes(_.flatten(this.submittedGuessNumbers), number);
         },
     }
 }
