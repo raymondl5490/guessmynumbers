67dea42edf1ba11642a87131b0d0b0236e63fbb7
raymondkdev <raymondkdev@gmail.com>
2022-07-07 19:48:58 -0400

    feat: feadback on UI

--- resources/js/components/modals/HelpModalComponent.vue
+++ resources/js/components/modals/HelpModalComponent.vue
@@ -7,35 +7,35 @@
             </h1>
         </template>
         <template #content>
-            <p class="my-8">
+            <p class="my-4 -mt-4">
                 You have three tries to guess the featured number.
                 Press the check mark to submit.
             </p>
 
-            <p class="my-8">
+            <p class="my-4">
                 After each guess, the colors will change to show how close you got!
             </p>
 
-            <p class="my-8">
+            <p class="my-4">
                 If you guess correctly, <span class="font-bold">you have a chance to submit your own numbers to be featured!</span>
             </p>
 
-            <p class="my-8">
+            <p class="my-4">
                 <img :src="asset('images/help_green.png')" class="w-2/3" />
                 <span class="font-bold">Green</span> indicates the number is in the proper spot.
             </p>
 
-            <p class="my-8">
+            <p class="my-4">
                 <img :src="asset('images/help_yellow.png')" class="w-2/3" />
                 <span class="font-bold">Yellow</span> indicates the number is correct, but in the wrong spot.
             </p>
 
-            <p class="my-8">
+            <p class="my-4">
                 <img :src="asset('images/help_double.png')" class="w-2/3" />
                 A <span class="font-bold">Purple</span> line indicates the number is used multiple times.
             </p>
 
-            <div class="flex justify-center my-8">
+            <div class="flex justify-center my-4">
                 <el-button type="success" size="large" :tabindex="-1" @click="value = false">GOT IT, LET'S PLAY</el-button>
             </div>
 
