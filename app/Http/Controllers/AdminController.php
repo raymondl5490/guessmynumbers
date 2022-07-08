12ef2ad32e8d96e58253438b2934ba8632732dfe
Raymond <raymondl5490@gmail.com>
2022-07-08 11:29:09 -0400

    change password

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
 
 class AdminController extends Controller
 {
@@ -24,5 +26,31 @@ class AdminController extends Controller
     public function index()
     {
         return view('admin');
+    }
+
+    public function changePassword()
+    {
+        return view('auth.passwords.change');
+    }
+
+    public function updatePassword(Request $request)
+    {
+        # Validation
+        $request->validate([
+            'old_password' => 'required',
+            'new_password' => 'required|confirmed',
+        ]);
+
+        #Match The Old Password
+        if (!Hash::check($request->old_password, auth()->user()->password)) {
+            return back()->with("error", "Old Password Doesn't match!");
+        }
+
+        #Update the new Password
+        User::whereId(auth()->user()->id)->update([
+            'password' => Hash::make($request->new_password)
+        ]);
+
+        return back()->with("status", "Password changed successfully!");
     }
 }
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -24,8 +24,8 @@
     <div id="app">
         <!-- This example requires Tailwind CSS v2.0+ -->
         <div class="relative bg-white">
-            <div class="max-w-7xl mx-auto px-4 sm:px-6">
-                <div class="flex justify-between items-center border-b-2 border-gray-100 py-6 md:justify-start md:space-x-10">
+            <div class="px-4 mx-auto max-w-7xl sm:px-6">
+                <div class="flex items-center justify-between py-6 border-b-2 border-gray-100 md:justify-start md:space-x-10">
                     <div class="flex justify-start lg:w-0 lg:flex-1">
                         <a href="{{ url('/') }}">
                             {{ config('app.name', 'Guess My Numbers') }}
@@ -34,22 +34,22 @@
                             </span>
                         </a>
                     </div>
-                    <div class="-mr-2 -my-2 md:hidden">
-                        <button type="button" class="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500" aria-expanded="false">
+                    <div class="-my-2 -mr-2 md:hidden">
+                        <button type="button" class="inline-flex items-center justify-center p-2 text-gray-400 bg-white rounded-md hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500" aria-expanded="false">
                             <span class="sr-only">Open menu</span>
                             <!-- Heroicon name: outline/menu -->
-                            <svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" aria-hidden="true">
+                            <svg class="w-6 h-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" aria-hidden="true">
                                 <path stroke-linecap="round" stroke-linejoin="round" d="M4 6h16M4 12h16M4 18h16" />
                             </svg>
                         </button>
                     </div>
-                    <div class="hidden md:flex items-center justify-end md:flex-1 lg:w-0">
+                    <div class="items-center justify-end hidden space-x-4 md:flex md:flex-1 lg:w-0">
                         @guest
                             @if (Route::has('login'))
-                                <a href="{{ route('login') }}" class="whitespace-nowrap text-base font-medium text-gray-500 hover:text-gray-900"> Sign in </a>
+                                <a href="{{ route('login') }}" class="text-base font-medium text-gray-500 whitespace-nowrap hover:text-gray-900"> Sign in </a>
                             @endif
                         @else
-                            <p class="whitespace-nowrap text-base font-medium text-gray-500 mx-2 font-light">
+                            <p class="text-base font-medium text-gray-500 whitespace-nowrap">
                                 Hello, {{ Auth::user()->name }}!
                             </p>
 
@@ -63,6 +63,11 @@
                                 <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
                                     @csrf
                                 </form>
+                            </div>
+                            <div class="text-base font-medium text-gray-500 hover:text-gray-900">
+                                <a class="dropdown-item" href="{{ route('change-password') }}">
+                                    {{ __('Change Password') }}
+                                </a>
                             </div>
                         @endguest
                     </div>
@@ -79,17 +84,17 @@
                 From: "opacity-100 scale-100"
                 To: "opacity-0 scale-95"
             -->
-            <div class="absolute top-0 inset-x-0 p-2 transition transform origin-top-right md:hidden">
-                <div class="rounded-lg shadow-lg ring-1 ring-black ring-opacity-5 bg-white divide-y-2 divide-gray-50">
-                    <div class="pt-5 pb-6 px-5">
+            <div class="absolute inset-x-0 top-0 p-2 transition origin-top-right transform md:hidden">
+                <div class="bg-white divide-y-2 rounded-lg shadow-lg ring-1 ring-black ring-opacity-5 divide-gray-50">
+                    <div class="px-5 pt-5 pb-6">
                         @guest
                             @if (Route::has('login'))
-                                <p class="mt-6 text-center text-base font-medium text-gray-500">
+                                <p class="mt-6 text-base font-medium text-center text-gray-500">
                                     <a href="{{ route('login') }}" class="text-indigo-600 hover:text-indigo-500"> Sign in </a>
                                 </p>
                             @endif
                         @else
-                            <p class="whitespace-nowrap text-base font-medium text-gray-500 hover:text-gray-900">
+                            <p class="text-base font-medium text-gray-500 whitespace-nowrap hover:text-gray-900">
                                 {{ Auth::user()->name }}
                             </p>
 
@@ -103,6 +108,11 @@
                                 <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
                                     @csrf
                                 </form>
+                            </div>
+                            <div class="text-base font-medium text-gray-500 hover:text-gray-900">
+                                <a class="dropdown-item" href="{{ route('change-password') }}">
+                                    {{ __('Change Password') }}
+                                </a>
                             </div>
                         @endguest
                     </div>
