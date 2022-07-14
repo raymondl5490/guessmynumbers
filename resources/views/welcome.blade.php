5489f8e138e58683460d11308ee303428342fb6b
raymondkdev <raymondkdev@gmail.com>
2022-07-14 19:50:15 +0200

    fix imgur url for link preview

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,15 +6,15 @@
 
     <!-- Link preview for Facebook, Skype -->
     <meta property="og:title" content="Guess My Numbers - A Daily Numbers Game" />
-    <meta property="og:type" content="website" />
+    <meta property="og:type" content="summary_large_image" />
     <meta property="og:url" content="https://guessmynumbers.com" />
-    <meta property="og:image" content="https://imgur.com/a/kRPgdkZ" />
+    <meta property="og:image" content="https://i.imgur.com/qy5Yezj.jpeg" />
     <meta property="og:description" content="Can you guess my three digit number? Updated twice a day." />
     <!-- Link preview for Twitter -->
     <meta property="twitter:title" content="Guess My Numbers - A Daily Numbers Game" />
     <meta property="twitter:card" content="summary_large_image" />
     <meta property="twitter:site" content="https://guessmynumbers.com" />
-    <meta property="twitter:image" content="https://imgur.com/a/kRPgdkZ" />
+    <meta property="twitter:image" content="https://i.imgur.com/qy5Yezj.jpeg" />
     <meta property="twitter:description" content="Can you guess my three digit number? Updated twice a day." />
 
     <title>{{ config('app.name') }}</title>
