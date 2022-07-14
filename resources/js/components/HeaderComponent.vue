2f7b54ecfe4060bb4173ccc6c40f4ed4198b0a6d
raymondkdev <raymondkdev@gmail.com>
2022-07-14 12:51:14 +0200

    submitted by line on its own row; replace placeholder on VipModal

--- resources/js/components/HeaderComponent.vue
+++ resources/js/components/HeaderComponent.vue
@@ -31,17 +31,15 @@
                 </div>
             </p>
             <p v-else class="mt-3 text-lg text-center text-gray-400 sm:mt-6 sm:text-xl md:text-4xl">
-                <p class="flex flex-wrap items-center justify-center">
-                    <p class="whitespace-pre">
-                        SUBMITTED BY 
-                    </p>
-                    <p class="flex font-bold text-green-700 flex-nowrap">
-                        <a :href="currentGame.link" target="_blank" class="underline">
-                            {{currentGame.author_name}}
-                        </a>
-                        , {{currentGame.author_location}}
-                        <img class="inline w-6 h-6" src="https://img.icons8.com/color/48/FAB005/approval--v3.png"/>
-                    </p>
+                <p class="whitespace-pre">
+                    Submitted by 
+                </p>
+                <p class="flex justify-center font-bold text-green-700 flex-nowrap">
+                    <a :href="currentGame.link" target="_blank" class="underline">
+                        {{currentGame.author_name}}
+                    </a>
+                    , {{currentGame.author_location}}
+                    <img class="inline w-6 h-6" src="https://img.icons8.com/color/48/FAB005/approval--v3.png"/>
                 </p>
                 <p class="font-bold text-blue-900">
                     {{formatTimestamp2Date(currentGame.created_at)}}
