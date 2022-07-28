b5697e96d7b9716ff82f7d43b6715ada2c88c7df
Raymond <raymondl5490@gmail.com>
2022-07-28 12:58:23 +0200

    feat(welcom.blade.php): Prevent unwanted zoom in/out

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,7 +2,7 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no">
 
     <!-- Link preview for Facebook, Skype -->
     <meta property="og:title" content="Guess My Numbers - A Daily Numbers Game" />
