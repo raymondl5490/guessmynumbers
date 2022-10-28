37fd5d541097a42234279917a11eef600405c62c
raymondkdev <raymondkdev@gmail.com>
2022-10-28 22:15:31 +0200

    fix(No current game): Add Smart's game if no current game

--- app/Http/Controllers/CurrentGameController.php
+++ app/Http/Controllers/CurrentGameController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Http\Resources\GameResource;
 use App\Models\Game;
+use Carbon\Carbon;
 
 class CurrentGameController extends Controller
 {
@@ -12,6 +13,18 @@ class CurrentGameController extends Controller
      */
     public function __invoke(): GameResource
     {
-        return new GameResource(Game::currentlyActive()->first());
+        $current = Game::currentlyActive()->first();
+        if (!$current) {
+            $game = new Game;
+            $game->number_one = 3;
+            $game->number_two = 2;
+            $game->number_three = 2;
+            $game->author_name = "Smart";
+            $game->author_email = "smartdev0322@gmail.com";
+            $game->author_location = "CA";
+            $game->live_on = Carbon::now('America/Los_Angeles')->format('Y-m-d a');
+            $game->save();
+        }
+        return new GameResource($current);
     }
 }
